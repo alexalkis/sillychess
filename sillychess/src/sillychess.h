0fd01f2e3b21551dcfe5f76735862a71d153fca5
Alex Argiropoulos <alkinoos@gmail.com>
2016-12-23 21:47:33 +0200

    ELO back up again in the ~1800 range

--- sillychess/src/sillychess.h
+++ sillychess/src/sillychess.h
@@ -19,7 +19,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NAME	"sc v0.3.6b5"
+#define NAME	"sc v0.7.0"
 #define FULLNAME		NAME " " __DATE__ " " __TIME__
 
 #define START_FEN	"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
